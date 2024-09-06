95ca9d6e13be97efdc675bd9f14175af176ca4a7
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2024-09-06 10:48:54 +0530

    Basic encapsulation of data and methods in a single class 'Employee'.

---
+++ src/com/wageCalculator/Employee.java
@@ -0,0 +1,10 @@
+package com.wageCalculator;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (extraHours * hourlyRate);
+    }
+}
