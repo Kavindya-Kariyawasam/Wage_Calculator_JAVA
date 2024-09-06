090a6ee9e361af23845683657a218f204230d07b
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2024-09-06 20:08:49 +0530

    Creating static fields and methods to track the number of employees.

--- src/com/wageCalculator/Employee.java
+++ src/com/wageCalculator/Employee.java
@@ -4,13 +4,22 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public static int numberOfEmployees;    //Declaring a static field.
+    // These are used when the value is independent of the objects where it is shared across all objects.
+
     public Employee(int baseSalary, int hourlyRate) {   //Here we have created a custom constructor instead of default constructor created by JAVA compiler
         setBaseSalary(baseSalary);
         setHourlyRate(hourlyRate);
+        numberOfEmployees++;    //This increments the number of employees each time we create an Employee object.
     }
     public Employee(int baseSalary) {
         this(baseSalary,0);
     }
+
+    public static void printNumberOfEmployees() {    //Declaring a static method.
+        System.out.println("Number of employees : " + numberOfEmployees);
+    }
+
 
     public int calculateWage(int extraHours) {
         return baseSalary + (extraHours * hourlyRate);
--- src/com/wageCalculator/Main.java
+++ src/com/wageCalculator/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var employee = new Employee(50_000, 20);
         int wage = employee.calculateWage(10);
+        Employee.printNumberOfEmployees();
         System.out.println("Wage : " + wage);
     }
 }
